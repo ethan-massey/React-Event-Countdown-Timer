3c8e134454fd47fbc8d29db782cc54f03bc16572
Ethan Massey <ejm4aa@virginia.edu>
2021-02-19 22:13:07 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,118 @@
+import React, { useState } from "react";
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
+
+const TenDaysFromNow = () => {
+  var today = new Date();
+  today.setDate(today.getDate() + 10); 
+  return new Date(today.getFullYear(), today.getMonth(), today.getDay(), 0, 0, 0);
+}
+
 
 export default function Home() {
+  // for event name input
+  const handleNameChange = (event) => {
+    setEventDetails({...eventDetails, Name: event.target.value});
+  }
+
+  // for date or time input
+  const handleDateTimeChange = (date) => {
+    console.log(date);
+    setEventDetails({...eventDetails, Date: date});
+  }
+
+  const [eventDetails, setEventDetails] = useState({Name: 0, Date: TenDaysFromNow()})
+  const [userInputForms, setUserInputForms] = useState({
+    NameForm: <TextField label="Event Name" name="Name" variant="outlined" onChange={handleNameChange}/>,
+    DateForm: <KeyboardDatePicker
+    disableToolbar
+    variant="inline"
+    format="MM/dd/yyyy"
+    margin="normal"
+    id="date-picker-inline"
+    label="Event Date"
+    name="Date"
+    value={eventDetails.Date}
+    onChange={handleDateTimeChange}
+    KeyboardButtonProps={{
+      'aria-label': 'change date',
+    }}/>,
+    TimeForm: <KeyboardTimePicker
+    margin="normal"
+    id="time-picker"
+    label="Event Time (Optional)"
+    name="Time"
+    value={eventDetails.Date}
+    onChange={handleDateTimeChange}
+    KeyboardButtonProps={{
+      'aria-label': 'change time',
+    }}/>
+  })
+
+  const validateUserInputs = () => {
+    // validate name
+    if(eventDetails.Name == ""){
+      setUserInputForms({...userInputForms, NameForm: <TextField label="Event Name" error name="Name" helperText="Event name can't be empty." variant="outlined" onChange={handleNameChange}/>})
+    }
+    // validate date & time
+    var today = new Date();
+    if(eventDetails.Date <= today){
+      setUserInputForms({...userInputForms, 
+        DateForm: <KeyboardDatePicker
+        disableToolbar
+        error
+        helperText="Event date must be in the future."
+        variant="inline"
+        format="MM/dd/yyyy"
+        margin="normal"
+        id="date-picker-inline"
+        label="Event Date"
+        name="Date"
+        value={eventDetails.Date}
+        onChange={handleDateTimeChange}
+        KeyboardButtonProps={{
+          'aria-label': 'change date',
+        }}/>})
+    }
+  }
+
+  // upon submitting event
+  const handleSubmit = () => {
+    // validate
+    var isValidInput = validateUserInputs();
+    // now get to change to timer view somehow
+    if (isValidInput) {
+
+    // show error messages over input
+    }else{
+
+    }
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div>
+      <h1>Countdown timer</h1>
+      <h4>Enter event details below.</h4>
+      <form>
+        {userInputForms.NameForm}
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          {userInputForms.DateForm}
+          {userInputForms.TimeForm}
+        </MuiPickersUtilsProvider>
+        <Button variant="contained" color="primary" onClick={handleSubmit}>
+          Start Timer!
+        </Button>
+      </form>
     </div>
   )
 }
