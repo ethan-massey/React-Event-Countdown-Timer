11babc9ea52fcc4078064ab9eb31626354a2421f
Ethan Massey <ejm4aa@virginia.edu>
2021-02-20 14:11:54 -0500

    added timer countdown

--- pages/Clock.js
+++ pages/Clock.js
@@ -1,10 +1,43 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import Button from '@material-ui/core/Button';
 
 export default function Clock(props){
+    const [difference, setDifference] = useState(props.eventDetails.Date - (new Date()));
+    
+    const getReadableTimeFromMilliseconds = (milli) => {
+        var totalTime = milli;
+        var days = Math.floor(milli/1000/60/60/24);
+        if(days > 0){
+            milli -= days*1000*60*60*24
+        }
+        var hours = Math.floor(milli/1000/60/60);
+        if(hours > 0){
+            milli -= hours*1000*60*60
+        }
+        var minutes = Math.floor(milli/1000/60);
+        if(minutes > 0){
+            milli -= minutes*1000*60
+        }
+        var seconds = Math.floor(milli/1000);
+        return (
+            <div>
+                <h1>{days} Days, and</h1>
+                <h1>{("0" + hours).slice(-2)}:{("0" + minutes).slice(-2)}:{("0" + seconds).slice(-2)}</h1>
+            </div>
+        )
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setDifference(props.eventDetails.Date - (new Date()))
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <div>
+            {getReadableTimeFromMilliseconds(difference)}
+            <h1>until {props.eventDetails.Name}</h1>
             <Button variant="contained" color="secondary" onClick={()=>{props.onClickBack()}}>BACK</Button>
         </div>
     )
