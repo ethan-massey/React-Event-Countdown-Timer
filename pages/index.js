b817be1579325917faec24ae24c1119e3e0441c5
Ethan Massey <ejm4aa@virginia.edu>
2021-02-21 19:40:59 -0500

    added title to app

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,7 @@ export default function App() {
   return (
     <div>
       <Head>
+        <title>Event Countdown</title>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
